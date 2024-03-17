6fb7a51d40cf910e3bc2070ac2eb5ecac3fe199a
Himanshu <guptarajeshwarionejan@gmail.com>
2024-03-17 14:09:58 +0530

    update

--- Section 2/frontend/src/app/comment/page.jsx
+++ Section 2/frontend/src/app/comment/page.jsx
@@ -3,65 +3,58 @@ import React, { useState } from 'react'
 // how to add font style in comments text
 const Comments = () => {
   const [taskArray, settaskArray] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    if (count === 1) {
+      setCount(count - 1);
+    }
+    else {
+      setCount(count + 1);
+    }
+  }
   const addnewtask = (e) => {
     //console.log(e.code);
-    if(e.code === 'Enter'){
-        console.log(e.target.value);
-        const obj = {text: e.target.value, completed: false};
-        settaskArray([obj, ...taskArray]) //... spread operator
-        e.target.value='';
+    if (e.code === 'Enter') {
+      console.log(e.target.value);
+      const obj = { text: e.target.value, completed: false };
+      settaskArray([obj, ...taskArray]) //... spread operator
+      e.target.value = '';
 
     }
   }
-  function handleDelete(task){
+  function handleDelete(task) {
     //console.log("delete clicked for", task);
     settaskArray(taskArray.filter((todo) => todo !== task));
   }
-    function MyButton() {
-      const [count, setCount] = useState(0);
-    
-      function handleClick() {
-        if(count === 1){
-          setCount(count - 1);
-        }
-        else{
-          setCount(count + 1);
-        }
-      }
-    
-      return (
-        <button onClick={handleClick} className='btn btn-primary'>
-          like {count} 
-        </button>
-      )
-    }
-   
-  
-
 
   return (
     <div className='vh-100 bg-secondary'>
-        <div className='container py-3'>
-            <h1 style={{ color : 'white', textAlign: 'center', fontSize:30 }}>Comments</h1>
-            <div className='card shadow p-4'>
-               <div className='card-header'>Enter comments {taskArray.length}
-              <input onKeyDown={addnewtask} type="text" maxLength='60' className='form-control border-3 border-secondary' />
-               </div>
-              <div className='card-body' >
-              {
-                        taskArray.map((task, index) => {return <div  className=' justify-content-between p-3' key={index}>
-                        <p className='m-0 h5'>{task.text}</p>    
-                        <MyButton/>         
-                        {/* <MyButton onClick={() => onclick(handleClick)} className='btn btn-primary mx-4'>like</MyButton> */}
-                        <button onClick={() => handleDelete(task)} className='btn btn-secondary mx-4'>delete</button>
-                        </div> } )
-                    }
-             </div>
+      <div className='container py-3'>
+        <h1 style={{ color: 'white', textAlign: 'center', fontSize: 30 }}>Comments</h1>
+        <div className='card shadow p-4'>
+          <div className='card-header'>Enter comments {taskArray.length}
+            <input onKeyDown={addnewtask} type="text" maxLength='60' className='form-control border-3 border-secondary' />
+          </div>
+          <div className='card-body' >
+            {
+              taskArray.map((task, index) => {
+                return <div className=' justify-content-between p-3' key={index}>
+                  <p className='m-0 h5'>{task.text}</p>
+                  <button onClick={handleClick} className='btn btn-primary'>
+                    like {count}
+                  </button>
+                  {/* <button onClick={handleClick} className='btn btn-primary mx-4'>like{count}</button>  */}
+                  <button onClick={() => handleDelete(task)} className='btn btn-secondary mx-4'>delete</button>
+                </div>
+              })
+            }
+          </div>
 
-            </div>
+        </div>
 
-        </div >
-     </div>
+      </div >
+    </div>
   )
 }
 
--- Section 2/frontend/src/app/signup/page.jsx
+++ Section 2/frontend/src/app/signup/page.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='container-fluid row align-items-center vh-100'>
+      <div className='header'>
+        <div className='text'>signup</div>
+        <div className='underline'></div>
+      </div>
+      <div className='inputs'>
+        <div className='input'>
+          <img src="" alt="" />
+          <input type="text" />
+        </div>
+        <div className='input'>
+          <img src="" alt="" />
+          <input type="email" />
+        </div>
+        <div className='input'>
+          <img src="" alt="" />
+          <input type="password" />
+        </div>
+      </div>
+      <div className="forget-password">lost password? <span>click here!</span></div>
+      <div className="submit-container">
+        <div className="submit">signup</div>
+        <div className='submit'>login</div>
+      </div>
+    </div>
   )
 }
 
